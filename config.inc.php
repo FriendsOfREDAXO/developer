42ed0b488d964cd17c4b07683a58032c2f5a2e35
Gregor Harlan <gharlan@web.de>
2010-11-25 17:59:22 +0100

    Erst nach ADDONS_INCLUDED synchronisieren. Closes #1

--- config.inc.php
+++ config.inc.php
@@ -3,7 +3,6 @@
 $mypage = 'developer';
 
 $REX['ADDON']['rxid'][$mypage] = '338';
-$REX['ADDON']['page'][$mypage] = $mypage;
 
 if ($REX['REDAXO']) {
   $I18N->appendFile($REX['INCLUDE_PATH'].'/addons/'.$mypage.'/lang/');
@@ -12,7 +11,7 @@ if ($REX['REDAXO']) {
 
 $REX['ADDON']['perm'][$mypage] = "admin[]";
 $REX['ADDON']['author'][$mypage] = 'Gregor Harlan';
-$REX['ADDON']['version'][$mypage] = '2.1';
+$REX['ADDON']['version'][$mypage] = '2.2.0';
 
 require_once dirname(__FILE__) .'/settings.inc.php';
 
@@ -20,15 +19,21 @@ if ($REX['ADDON']['settings']['developer']['templates']
   || $REX['ADDON']['settings']['developer']['modules']
   || $REX['ADDON']['settings']['developer']['actions'])
 {
-  $loggedIn = true;
-  if (!isset($REX['LOGIN']) || !$REX['LOGIN'])
+  rex_register_extension('ADDONS_INCLUDED', 'rex_developer_start');
+  
+  function rex_developer_start($params)
   {
-    $REX['LOGIN'] = new rex_backend_login($REX['TABLE_PREFIX'] .'user');
-    $loggedIn = $REX['LOGIN']->checkLogin();
-  }
-  if ($loggedIn && $REX['LOGIN']->USER->isAdmin())
-  {
-    require_once $REX['INCLUDE_PATH'] .'/addons/developer/classes/class.rex_developer_manager.inc.php';
-    rex_developer_manager::sync();
+    global $REX;
+    $loggedIn = true;
+    if (!isset($REX['LOGIN']) || !$REX['LOGIN'])
+    {
+      $REX['LOGIN'] = new rex_backend_login($REX['TABLE_PREFIX'] .'user');
+      $loggedIn = $REX['LOGIN']->checkLogin();
+    }
+    if ($loggedIn && $REX['LOGIN']->USER->isAdmin())
+    {
+      require_once $REX['INCLUDE_PATH'] .'/addons/developer/classes/class.rex_developer_manager.inc.php';
+      rex_developer_manager::sync();
+    }
   }
 }
