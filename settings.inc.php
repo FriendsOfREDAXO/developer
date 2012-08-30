08212c4620e80e46fbf5b4608301e9bc4b2bf77b
jdlx <github@rexdev.de>
2012-08-30 03:33:19 +0300

    default: alle syncs "on"..

--- settings.inc.php
+++ settings.inc.php
@@ -4,8 +4,8 @@ $REX['ADDON']['dirperm']['developer'] = $REX['DIRPERM'];
 $REX['ADDON']['fileperm']['developer'] = $REX['FILEPERM'];
 
 // --- DYN
-$REX['ADDON']['settings']['developer']['templates'] = "0";
-$REX['ADDON']['settings']['developer']['modules'] = "0";
-$REX['ADDON']['settings']['developer']['actions'] = "0";
+$REX['ADDON']['settings']['developer']['templates'] = "1";
+$REX['ADDON']['settings']['developer']['modules'] = "1";
+$REX['ADDON']['settings']['developer']['actions'] = "1";
 $REX['ADDON']['settings']['developer']['dir'] = "developer_files";
 // --- /DYN
