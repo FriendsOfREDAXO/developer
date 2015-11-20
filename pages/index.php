014e12b651d4df96a937acbaf013006e9c33814f
Gregor Harlan <mail@gh01.de>
2015-11-20 16:17:48 +0100

    Anpassung an aktuelle R5-Version

--- pages/index.php
+++ pages/index.php
@@ -18,13 +18,7 @@ if (rex_post('config-submit', 'boolean')) {
     echo rex_view::success($this->i18n('saved'));
 }
 
-$content = '';
-
-$content .= '
-<div class="rex-form">
-    <form action="' . rex_url::currentBackendPage() . '" method="post">
-        <fieldset>
-            <h2>' . $this->i18n('settings') . '</h2>';
+$content = '<fieldset>';
 
 $formElements = [];
 
@@ -70,18 +64,22 @@ $content .= $fragment->parse('core/form/checkbox.php');
 $formElements = [];
 
 $n = [];
-$n['field'] = '<button class="rex-button" type="submit" name="config-submit" value="1" ' . rex::getAccesskey($this->i18n('save'), 'save') . '>' . $this->i18n('save') . '</button>';
+$n['field'] = '<button class="btn btn-save rex-form-aligned" type="submit" name="config-submit" value="1" ' . rex::getAccesskey($this->i18n('save'), 'save') . '>' . $this->i18n('save') . '</button>';
 $formElements[] = $n;
 
+$fragment = new rex_fragment();
+$fragment->setVar('flush', true);
+$fragment->setVar('elements', $formElements, false);
+$buttons = $fragment->parse('core/form/submit.php');
 
 $fragment = new rex_fragment();
-$fragment->setVar('elements', $formElements, false);
-$content .= $fragment->parse('core/form/submit.php');
+$fragment->setVar('class', 'edit');
+$fragment->setVar('title', $this->i18n('settings'));
+$fragment->setVar('body', $content, false);
+$fragment->setVar('buttons', $buttons, false);
+$content = $fragment->parse('core/page/section.php');
 
-$content .= '
-        </fieldset>
-
-    </form>
-</div>';
-
-echo rex_view::contentBlock($content, '', 'block');
+echo '
+    <form action="' . rex_url::currentBackendPage() . '" method="post">
+        ' . $content . '
+    </form>';
