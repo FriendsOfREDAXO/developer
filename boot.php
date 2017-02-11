95fb258c61ee6f3e7008902439b312d18fa36083
Markus Staab <maggus.staab@googlemail.com>
2017-02-11 20:13:34 +0100

    dont trigger sync mechnisms in image requests, so we dont acquire a session-lock in media-manager requests

--- boot.php
+++ boot.php
@@ -1,5 +1,11 @@
 <?php
 
+if (!empty($_SERVER['HTTP_ACCEPT']) && stripos($_SERVER['HTTP_ACCEPT'], 'image/') !== false) {
+    // dont trigger sync mechnisms in image requests, so we dont acquire a session-lock
+    // in media-manager requests
+    return;
+}
+
 if (!rex::isBackend() && $this->getConfig('sync_frontend') || rex::getUser()) {
     rex_extension::register('PACKAGES_INCLUDED', function () {
         if (($user = rex_backend_login::createUser()) && $user->isAdmin()) {
