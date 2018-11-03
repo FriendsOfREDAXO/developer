397ac5021c0f827d6fa98c3831472bd6e05ba5b7
Gregor Harlan <mail@gh01.de>
2018-11-03 13:25:33 +0100

    Editor-URL liefern

--- boot.php
+++ boot.php
@@ -18,3 +18,38 @@ if (
         }
     });
 }
+
+rex_extension::register('EDITOR_URL', function (rex_extension_point $ep) {
+    if (!preg_match('@^rex:///(template|module|action)/(\d+)(?:/([^/]+))?@', $ep->getParam('file'), $match)) {
+        return null;
+    }
+
+    $type = $match[1];
+    $id = $match[2];
+
+    if (!$this->getConfig($type.'s')) {
+        return null;
+    }
+
+    if ('template' === $type) {
+        $subtype = 'template';
+    } elseif (!isset($match[3])) {
+        return null;
+    } else {
+        $subtype = $match[3];
+    }
+
+    $path = rtrim(rex_developer_manager::getBasePath(), '/\\').'/'.$type.'s';
+
+    if (!$files = rex_developer_synchronizer::glob("$path/*/$id.rex_id", GLOB_NOSORT)) {
+        return null;
+    }
+
+    $path = dirname($files[0]);
+
+    if (!$files = rex_developer_synchronizer::glob("$path/*$subtype.php", GLOB_NOSORT)) {
+        return null;
+    }
+
+    return rex_editor::factory()->getUrl($files[0], $ep->getParam('line'));
+}, rex_extension::LATE);
